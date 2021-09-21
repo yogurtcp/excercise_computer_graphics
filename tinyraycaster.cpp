6bee33a2c0051b0fc6908e42d14574e9760e6238
Yevgeny <yogurt.co@gmail.com>
2021-09-21 15:51:03 +0300

    working version with openCV

--- tinyraycaster.cpp
+++ tinyraycaster.cpp
@@ -8,45 +8,34 @@
 
 #include <iostream>
 
-// #include <opencv2/opencv.hpp>
-
-// using namespace cv;
-
-// int main( int argc, char** argv )
-// {
-//     std::cout << "aa" << std::endl;
- 
-//     std::string img = "lenna.jpg";
-//     Mat srcImage = imread(img);
-//     if (!srcImage.data) {
-//         return 1;
-//     }
-//     imshow("srcImage", srcImage);
-//     waitKey(0);
-//     return 0;
-// }
+#include <opencv2/opencv.hpp>
 
 void render() {
     const int width    = 1024;
     const int height   = 768;
-    std::vector<Vec3f> framebuffer(width*height);
+    // std::vector<Vec3f> framebuffer(width*height);
 
-    for (size_t j = 0; j<height; j++) {
-        for (size_t i = 0; i<width; i++) {
-            // framebuffer[i+j*width] = Vec3f(j/float(height),i/float(width), 0);
-            framebuffer[i+j*width] = Vec3f(0,1, 0);
-        }
-    }
+    cv::Mat save_img(cv::Size(width, height), CV_8UC3, cv::Scalar(0,0,0));
 
-    std::ofstream ofs; // save the framebuffer to file
-    ofs.open("./out.ppm");
-    ofs << "P6\n" << width << " " << height << "\n255\n";
-    for (size_t i = 0; i < height*width; ++i) {
-        for (size_t j = 0; j<3; j++) {
-            ofs << (char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
-        }
+    for (size_t j = 0; j<width; j++) {
+        for (size_t i = 0; i<height; i++) {
+            save_img.at<cv::Vec3b>(i, j)[0] = 0;
+            save_img.at<cv::Vec3b>(i, j)[1] = 255*j/width;
+            save_img.at<cv::Vec3b>(i, j)[2] = 255*i/height;
+            }
     }
-    ofs.close();
+    std::string outPath = "test.png";
+    cv::imwrite(outPath, save_img);
+    // std::ofstream ofs; // save the framebuffer to file
+    // ofs.open("./out.ppm");
+    // ofs << "P6\n" << width << " " << height << "\n255\n";
+    // for (size_t i = 0; i < height*width; ++i) {
+    //     for (size_t j = 0; j<3; j++) {
+    //         ofs << (char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
+    //     }
+    // }
+    // ofs.close();
+    std::cout << "image saved to " <<  outPath;
 }
 
 int main() {
